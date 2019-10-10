607545f79b7e25478797d7da9fcb9d8e96f0034f
Wallenius Noora <s1900614@edu.bc.fi>
2019-10-10 20:17:59 +0300

    speedgame app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Circle from './Circle/Circle';
+import GameOver from './GameOver/GameOver';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const getRandomInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+class App extends Component {
+  state = {
+    score: 0,
+    current: 0,
+    showGameOver: false
+  };
+
+  pace = 1500;
+  timer = undefined;
+
+  next = () => {
+
+    let nextActive = undefined;
+
+    do {
+      nextActive = getRandomInteger(1, 4);
+    } while (nextActive === this.state.current);
+
+    this.setState({
+      current:nextActive
+    });
+
+    this.pace *= 0.95;
+    this.timer = setTimeout(this.next.bind(this), this.pace);
+
+    console.log(this.state.current);
+  }
+
+
+  clickHandler = (buttonId) => {
+
+    console.log('Button was clicked', buttonId);
+
+    if (this.state.current !== buttonId) {
+      this.stopHandler();
+      return;
+    }
+
+    this.setState({
+      score: this.state.score +1
+    });
+    /* Tai näin:
+    this.setState(prevState => {
+      return {
+        score: prevState.score + 1  // prevState on itse keksitty nimi ominaisuudelle eli nimi voi olla ihan mitä vaan
+      }})
+    */
+  }
+
+
+  startHandler = () => {
+    this.next();
+  }
+
+
+  stopHandler = () => {
+    clearTimeout(this.timer);
+    
+    this.setState( {
+      showGameOver: true
+    })
+  }
+ 
+
+  render () {
+    return (
+      <div>
+        <h1>Speedgame</h1>
+        <p className="Score">Score: {this.state.score}</p>
+
+          <Circle 
+          buttonColor='darkgoldenrod'
+          active={this.state.current === 1}
+          click={() => this.clickHandler(1)} 
+          />
+          <Circle 
+          buttonColor='darkred'
+          active={this.state.current === 2}
+          click={() => this.clickHandler(2)} 
+          />
+          <Circle 
+          buttonColor='darkolivegreen'
+          active={this.state.current === 3}
+          click={() => this.clickHandler(3)} 
+          />
+          <Circle 
+          buttonColor='darkblue'
+          active={this.state.current === 4}
+          click={() => this.clickHandler(4)} 
+          />
+
+        <div className="Buttons">
+            <button onClick={this.startHandler}>Start game</button>
+            <button onClick={this.stopHandler}>Stop game</button>
+        </div>  
+
+        {this.state.showGameOver && <GameOver score={this.state.score} />}
+      </div>
+    );
+  }
+};
+
 export default App;
+
+
---
+++ src/Speedgame/Speedgame.js
@@ -0,0 +1,45 @@
+/*
+import React, {Component} from 'react';
+import './Speedgame.css';
+
+
+class Speedgame extends Component {
+    state = {
+        score: 0
+    }
+    clickHandler = () => {
+        console.log("Circle was clicked");
+        this.setState({
+          score: this.state.score +1
+        });
+    }
+    
+    render () {
+      return (
+        <div className="container">
+            <h1>Speedgame</h1>
+            <p className="score">Score: {this.state.score}</p>
+            <div className="circles">
+                <div onClick={this.clickHandler} className="circle yellow"></div>
+                <div onClick={this.clickHandler} className="circle red"></div>
+                <div onClick={this.clickHandler} className="circle green"></div>
+                <div onClick={this.clickHandler} className="circle blue"></div>
+            </div>
+            <div className="buttons">
+                <button onClick={this.startHandler} className="start">Start game</button>
+                <button onClick={this.stopHandler} className="stop">Stop game</button>
+            </div>   
+        </div>
+      );
+    }
+};
+
+export default Speedgame;
+*/
+/*
+
+<div id="overlay">
+  <div id="gameover">
+  </div>
+</div>
+*/
